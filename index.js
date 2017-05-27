6b9c63c7014e28bac0f022ab8363f0fc6133170f
Pahud Hsieh <pahudnet@gmail.com>
2017-05-27 19:23:51 +0800

    init commit

---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict';
+exports.handler = (event, context, callback) => {
+    /*
+     * Extract request object in order to preserve httpVersion field.
+     * This is necessary in order to match the client's httpVersion. Please
+     * refer to your CloudFront Distribution's HttpVersion configuration for whether to
+     * specify HTTP 1.1, 2.0 or match-viewer.
+     */
+    const request = event.Records[0].cf.request;
+
+    /*
+     * Generate HTTP response using 200 status code with a simple body.
+     */
+    const response = {
+        status: '200',
+        statusDescription: 'HTTP OK',
+        httpVersion: request.httpVersion,
+        body: request.clientIp,
+        headers: {
+            "Vary": ['*'],
+        },
+    };
+
+    callback(null, response);
+};
